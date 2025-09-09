e29875dd3870bc5b4321945dc99b39e0e65a758a
Benji <ben.jonasson@outlook.com>
2025-09-09 18:27:56 +0200

    Add SettingsView component with initial SwiftUI implementation

---
+++ GPTme/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  GPTme
+//
+//  Created by Benji on 2025-09-09.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
